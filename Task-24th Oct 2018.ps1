de63147deaff6d95647c386a609cc31e653eb486
nashit1985 <44311474+nashit1985@users.noreply.github.com>
2018-10-24 11:56:58 +0530

    Add files via upload

---
+++ Task-24th Oct 2018.ps1
@@ -0,0 +1,24 @@
+﻿<This is for the Task assigned by Dinesh Sir, on 23rd Oct 2018. Task Completed by Nashit Sekhani on 24th Oct 2018>
+<"Task#1" This cmdlet is to find command which generate random number>
+Get-command -noun random
+<Output is Get-Random>
+Get-Random
+<"Task#2" This cmdlet is to find command which gets time>
+Get-command -name *date*
+<Output is Get-Date>
+Get-Date
+<"Task#3" Will run this command to find "Object Type" for Get-Date>
+Get-Help Get-Date
+<In description it says that "Get-Date" cmdlet gets "DateTime" object>
+<"Task#4" Will run this command to find information about hotfixes installed on local system>
+Get-Command -noun *Hotfix*
+<Output suggests "Get-Hotfix">
+Get-HotFix
+<"Task#5" Will run this cmdlet to find what cmdlet is used for extracting event logs>
+Get-Command -noun *event*
+<Output suggests "Get-Eventlog">
+<Will take further help to understand the syntax for this cmdlet, so as to extract only top 50 security log enteries> 
+Get-help Get-EventLog -Examples
+<One of the examples suggest this command, so I will go with this>
+Get-EventLog -Newest 50 -LogName "Security"| out-file -FilePath "c:\IAC\Task5.txt"
+<End of the Task>
